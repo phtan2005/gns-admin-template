2966e7f443597e3e67f4886ac7db1ff1078e44c5
phtan2005 <phamhuynh.tan04@gmail.com>
2026-07-15 16:53:37 +0700

    feat: add customer group member management

--- tests/customer-groups-pages.test.js
+++ tests/customer-groups-pages.test.js
@@ -77,3 +77,39 @@ test('customer-group-form.html exposes the create and edit form contract', () =>
   const app = source.indexOf('assets/js/app.js');
   assert.ok(bootstrap >= 0 && bootstrap < store && store < form && form < app);
 });
+
+test('customer-group-details.html exposes the detail and member management contract', () => {
+  const source = read('customer-group-details.html');
+
+  for (const asset of [
+    'assets/css/bootstrap.min.css',
+    'assets/css/icons.min.css',
+    'assets/css/app.min.css',
+    'assets/css/custom.css',
+    'assets/libs/sweetalert2/sweetalert2.min.css'
+  ]) assert.match(source, new RegExp(asset.replaceAll('.', '\\.')));
+
+  for (const id of [
+    'navbar-nav',
+    'topnav-hamburger-icon',
+    'group-title',
+    'group-description-text',
+    'group-status-badge',
+    'member-count',
+    'member-search',
+    'members-table-body',
+    'members-empty',
+    'edit-group-link'
+  ]) assert.match(source, new RegExp(`id=["']${id}["']`));
+
+  assert.match(source, /class=["'][^"']*main-content[^"']*["']/);
+  assert.match(source, /class=["'][^"']*page-content[^"']*["']/);
+  assert.match(source, /href=["']customer-groups[.]html["']/);
+
+  const bootstrap = source.indexOf('assets/libs/bootstrap/js/bootstrap.bundle.min.js');
+  const swal = source.indexOf('assets/libs/sweetalert2/sweetalert2.min.js');
+  const store = source.indexOf('assets/js/pages/customer-groups-store.js');
+  const details = source.indexOf('assets/js/pages/customer-group-details.js');
+  const app = source.indexOf('assets/js/app.js');
+  assert.ok(bootstrap >= 0 && bootstrap < swal && swal < store && store < details && details < app);
+});
